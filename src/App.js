0dc250a88f1999018c3a4d6ba1931920dec719c7
이태형 <spdhsrnvl123@naver.com>
2022-12-13 14:57:11 +0900

    🎉롤링페이퍼 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width:100%;
+  height : 100vh;
+  background-color: blue;
+`;
+const Children = styled.div`
+  max-width: 414px;
+  height: 100%;
+  background-color: red;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Father>
+        <Children>
+            <h1>롤링페이퍼</h1>
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+            안녕하세요!
+          </Children>
+      </Father>
     </div>
   );
 }
